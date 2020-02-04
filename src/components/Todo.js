e2cc8efdaaf75a8b590c7e1f932464a5347dec9d
Paige Niedringhaus <paigen11@gmail.com>
2020-02-04 14:18:58 -0500

    adding hooks state

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,9 +3,19 @@ import React, { useState } from 'react';
 const Todo = props => {
   const [todoName, setTodoName] = useState('');
   const [todoList, setTodoList] = useState([]);
+  // const [todoState, setTodoState] = useState({ userInput: '', todoList: [] });
 
   const inputChangeHandler = e => {
+    // setTodoState({ userInput: e.target.value, todoList: todoState.todoList });
     setTodoName(e.target.value);
+  };
+
+  const todoAddHandler = () => {
+    // setTodoState({
+    //   userInput: todoState.userInput,
+    //   todoList: [...todoState.todoList, todoState.todoName],
+    // });
+    setTodoList([...todoList, todoName]);
   };
 
   return (
@@ -16,8 +26,14 @@ const Todo = props => {
         onChange={inputChangeHandler}
         value={todoName}
       />
-      <button type="button">Add</button>
-      <ul></ul>
+      <button type="button" onClick={todoAddHandler}>
+        Add
+      </button>
+      <ul>
+        {todoList.map(todo => (
+          <li key={todo}>{todo}</li>
+        ))}
+      </ul>
     </>
   );
 };
