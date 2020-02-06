5b1edd5208c20920ed74e2b41db78fdaff91e3ce
Paige Niedringhaus <paigen11@gmail.com>
2020-02-06 17:09:34 -0500

    adding context

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Todo from './components/Todo';
+import Header from './components/Header';
+import Auth from './components/Auth';
+import AuthContext from './auth-context';
 
-function App() {
+const App = props => {
+  // const [page, setPage] = useState('auth');
+  const [showTodos, setShowToDos] = useState(true);
+  const [showAuth, setShowAuth] = useState(false);
+  const [authStatus, setAuthStatus] = useState(false);
+
+  // const switchPage = pageName => {
+  //   setPage(pageName);
+  // };
+
+  const todosHandler = () => {
+    setShowToDos(true);
+    setShowAuth(false);
+  };
+
+  const authHandler = () => {
+    setShowAuth(true);
+    setShowToDos(false);
+  };
+
+  const login = () => {
+    setAuthStatus(true);
+  };
+
   return (
-    <div>
-      <Todo />
+    <div className="App">
+      <AuthContext.Provider value={{ status: authStatus, login: login }}>
+        <Header onLoadTodos={todosHandler} onLoadAuth={authHandler} />
+        <hr />
+        {showTodos && <Todo />}
+        {showAuth && <Auth />}
+      </AuthContext.Provider>
     </div>
   );
-}
+};
 
 export default App;
